a592440798902ffc91096c10cb5509af3d878120
Emanuelamberti <elemanu92@yahoo.com>
2022-02-27 16:51:00 -0300

    New repository

---
+++ src/main/java/com/appDisney/application/Configurations/EmailConfigurations.java
@@ -0,0 +1,65 @@
+package com.appDisney.application.Configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+
+import javax.mail.internet.MimeMessage;
+import java.io.InputStream;
+
+@Configuration
+public class EmailConfigurations {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSender() {
+
+            @Override
+            public void send(SimpleMailMessage simpleMessage) throws MailException {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void send(SimpleMailMessage... simpleMessages) throws MailException {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public MimeMessage createMimeMessage(){
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+
+            @Override
+            public MimeMessage createMimeMessage(InputStream contentStream) throws MailException {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void send(MimeMessage mimeMessage) throws MailException {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void send(MimeMessage... mimeMessages) throws MailException {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void send(MimeMessagePreparator mimeMessagePreparator) throws MailException {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void send(MimeMessagePreparator... mimeMessagePreparators) throws MailException {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+        };
+
+    }
+
+}
